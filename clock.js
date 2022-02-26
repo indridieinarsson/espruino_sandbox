70a7e44723f196fafe832a45edb247220fc1d9c6
indridieinarsson <indridi.einarsson@gmail.com>
2022-02-26 15:38:03 +0000

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,20 +1,29 @@
 //ble = (function () {
-exports = {}
+// 1/4 : String.fromCharCode(188)
+// 2/4 : String.fromCharCode(189)
+// 3/4 : String.fromCharCode(190)
+exports = {};
+  exports.quarters = [0, String.fromCharCode(188), String.fromCharCode(189), String.fromCharCode(190), 0];
+
+  exports.compactTime = function(t) {
+    return ''+t.getHours()+this.quarters[Math.round(t.getMinutes()/15)];
+  };
 
   exports.draw = function draw (x,y, Radius, Settings) {
     let halfScreenWidth   = g.getWidth() / 2;
     let largeComplication = (x === halfScreenWidth);
-      let hands = require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clock-hands/main/ClockHands.js')
+      let hands = require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clock-hands/main/ClockHands.js');
 
     g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
     g.setFont('Vector', 18);
     g.setFontAlign(0,0);
 
     let today = new Date(), Text;
+    //this.compactTime(today);
     if (largeComplication) {
-      Text = today.getHours()+':'+String.fromCharCode(188);
+      Text = this.compactTime(today)+'-22'+String.fromCharCode(190);
     } else {
-      Text = '' + today.getDate()+'-';
+      Text = this.compactTime(today);
     }
 
     g.drawString(Text, x,y);
@@ -22,13 +31,13 @@ exports = {}
   };
 //})();
 
-let Clockwork = require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clockwork/main/Clockwork.js')
+let Clockwork = require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clockwork/main/Clockwork.js');
 
 Clockwork.windUp({
   size: require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clock-size/main/ClockSize.js'),
   complications: {
     r:require('https://raw.githubusercontent.com/rozek/banglejs-2-date-complication/main/Complication.js'),
     t:require('https://raw.githubusercontent.com/indridieinarsson/banglejs-2-date-complication/main/Complication.js'),
-      b:exports
+      l:exports
   }
 });
