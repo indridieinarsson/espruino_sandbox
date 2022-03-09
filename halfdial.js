ff44f27f83e1604f8e93d7e2798eb06bd1a6c8f3
indridieinarsson <indridi.einarsson@gmail.com>
2022-03-09 00:12:51 +0000

    Update halfdial.js

--- halfdial.js
+++ halfdial.js
@@ -1,27 +1,29 @@
 ;(function () {
 
 const NDots = 20;
-exports.draw = function draw (Settings, CenterX, CenterY, outerRadius, value) {
-function drawdot(i){
-    let Phi = (i+NDots/4) * Math.PI*2/NDots; // Phi from 1/4 x 2Pi -> 3/4x2Pi
-    let dotx = CenterX + outerRadius * Math.sin(Phi);
-    let doty = CenterY - outerRadius * Math.cos(Phi);
-    g.fillCircle(dotx,doty, 3);
-}
-function drawArm(i){
-    let Phi = (0.5+i)*Math.PI;
-    let dotx = CenterX - outerRadius*0.7 * Math.sin(Phi);
-    let doty = CenterY - outerRadius*0.7 * Math.cos(Phi);
-    let dotx2 = CenterX - outerRadius*0.95 * Math.sin(Phi);
-    let doty2 = CenterY - outerRadius*0.95 * Math.cos(Phi);
-    g.drawLine(dotx,doty,dotx2,doty2,);
-}
+const dphi = 2*Math.PI/50
 
-g.setColor('#FF0000');//red
-for (let i = 0; i <= NDots/2; i++) {
-    drawdot(i);
-}
-g.setColor('#000000');//black
-drawArm(value);
-}
-})();
+function draw (Settings, CenterX, CenterY, outerRadius, value) {
+  function drawdot(Phi){
+      let dotx = CenterX + outerRadius * Math.sin(Phi);
+      let doty = CenterY - outerRadius * Math.cos(Phi);
+      g.fillCircle(dotx,doty, 1.5);
+  }
+
+  function drawArm(i){
+      let Phi = Math.PI-10*dphi+20*dphi*value
+      let dotx = CenterX - outerRadius*0.7 * Math.sin(Phi);
+      let doty = CenterY - outerRadius*0.7 * Math.cos(Phi);
+      let dotx2 = CenterX - outerRadius*0.95 * Math.sin(Phi);
+      let doty2 = CenterY - outerRadius*0.95 * Math.cos(Phi);
+      g.drawLine(dotx,doty,dotx2,doty2);
+  }
+
+  g.setColor('#FFFFFF');//red
+  for (let i = 0; i <= 10; i++) {
+    drawdot(Math.PI+i*dphi);
+    drawdot(Math.PI-i*dphi);
+  }
+  g.setColor('#FFFFFF');//black
+  drawArm(value);
+  }})();
