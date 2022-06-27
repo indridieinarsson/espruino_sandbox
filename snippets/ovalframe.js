f86519f8c9d6da32245e2b9d3e5fe12ad6981585
indridieinarsson <indridi.einarsson@gmail.com>
2022-06-27 01:30:14 +0000

    Update ovalframe.js

--- snippets/ovalframe.js
+++ snippets/ovalframe.js
@@ -2,6 +2,26 @@ g.clear();
 //th = 3
 //x1=20, y1=43;
 //r = 21
+function find_split_segment(csseg, nrtf){
+  for (var i = 0; i < csseg.length; i++) {
+    if (nrtf < csseg[i]){
+      return i;
+    }
+  }
+}
+
+function cumsum(segs) {
+  retval = [];
+  segs.forEach(function(nr, ix){
+    if (ix==0) {
+      retval.push(nr);
+    }
+    else {
+      retval.push(nr+retval[ix-1]);
+    }
+  });
+  return retval;
+}
 
 function ur_corner(x1, y1, r, th){
   urcorn_o = [x1, y1, x1+r,y1, x1+r,y1+r];
@@ -26,9 +46,9 @@ function ul_corner(x1, y1, r, th){
 
 
 function thickring(x,y, r, th){
-  g.setColor(0,0,0);
+  //g.setColor(0x646464);
   g.fillCircle(x,y,r);
-  g.setColor(255,255,255);
+  g.setColor(0xFFFFFF);
   g.fillCircle(x,y,r-th-1);
 }
 h = g.getHeight();
@@ -40,71 +60,40 @@ ur_corner(20, 43, 21, 4);
 //ul_corner(70, 83, 21, 3);
 rad = 16;
 th=3;
-thickring(0+rad  ,0+rad  , rad, th);
-thickring(w-rad-2,0+rad  , rad, th);
-thickring(0+rad  ,h-rad-2, rad, th);
-thickring(w-rad-2,h-rad-2, rad, th);
+function topleft(){
+  thickring(0+rad  ,0+rad  , rad, th);
+}
+function topright(){
+  thickring(w-rad-2,0+rad  , rad, th);
+}
+function bottomleft(){
+  thickring(0+rad  ,h-rad-2, rad, th);
+}
+function bottomright(){
+  thickring(w-rad-2,h-rad-2, rad, th);
+}
+g.setColor(255,255,255);
+
 // bogalengd : 2*pi*rad/4 = 
 bl = Math.round(0.5*Math.PI*rad);
-//segments = []
-//segments.push(whalf-rad);
-bsegments = [];
 
-segments = [(whalf-rad), bl, (h-rad-rad), bl, (whalf-rad)];
-
-function cumsum(segs) {
-  retval = [];
-  segs.forEach(function(nr, ix){
-    if (ix==0) {
-      retval.push(nr);
-    }
-    else {
-      retval.push(nr+retval[ix-1]);
-    }
-  });
-  return retval;
-}
-
-cssegments = cumsum(segments);
-
-console.log("cumulative segment lengths");
-console.log(cssegments);
-console.log(segments);
 halflength = (whalf-rad) + bl + (h-rad-rad) + bl + (whalf-rad);
 n_hbl = Math.round((whalf-rad)/bl);
 n_bl = Math.round((w-2*rad)/bl);
-console.log("nr of corners in straight segments : " + n_hbl + " , " + n_bl);
-console.log("bogalengd :" + bl);
-console.log("total length :" + halflength);
 
 nrtofind = 200;
 tarcnr = n_hbl;
 barcnr = tarcnr + n_bl+1;
 console.log("arc indices " + tarcnr + " , "+barcnr);
-function find_split_segment(csseg, nrtf){
-  for (var i = 0; i < csseg.length; i++) {
-    if (nrtf < csseg[i]){
-      return i;
-    }
-  }
-}
-// fill rest o center
-g.fillRect(0+rad,0,     w-rad-2,h);
-g.fillRect(0    ,0+rad, w      ,h-rad-2);
-// Top segment:g.fillRect(0+rad,0, w-rad-2, th);
-g.setColor(128,128,0);
-g.fillRect(0+rad,0, whalf, th);
-g.setColor(255,0,0);
-g.fillRect(whalf,0, w-rad-2, th);
-top = [];
-//nrs = [0,1,2,3,4,5]
+
+
+
 nrs = [];
 for (var i=0; i<n_bl; i++){
   nrs.push(i);
 }
 
 l = w-2*rad-2
-segments = nrs.map(x => Math.round((x+1)*l/6));
 
 intervals = nrs.map(x => [rad+Math.round(x*l/6), rad+Math.round((x+1)*l/6)]);
 lengths = nrs.map(x => Math.round((x+1)*l/n_bl) - Math.round(x*l/n_bl));
@@ -117,8 +106,19 @@ all_lengths.push(bl);
 all_lengths=all_lengths.concat(lengths.slice().reverse().slice(n_hbl,n_bl));
 cumsum_all = cumsum(all_lengths);
 a = find_split_segment(cumsum_all, nrtofind);
-rtop = a>tarcnr;
-rbot = a>barcnr;
+
+g.setColor(0x646464);
+rtop = a<tarcnr;
+if (rtop) {g.setColor(128,128,0);}
+topright();
+rbot = a<barcnr;
+if (rbot) {g.setColor(128,128,0);} else {g.setColor(0x646464);}
+bottomright();
+console.log("rtop, rbot" + rtop + rbot);
+// fill rest of center
+g.setColor(255,255,255);
+g.fillRect(0+rad,0,     w-rad-2,h);
+g.fillRect(0    ,0+rad, w      ,h-rad-2);
 
 g.setColor(0,128,128);
 // Top
@@ -138,18 +138,3 @@ r_intervals.slice().reverse().forEach(it => {
   r_rects.push([it[0], h-th-2, it[1], h]);
 });
 r_rects.forEach(it => g.fillRect(it[0],it[1],it[2],it[3]));
-// Left
-//intervals.forEach(it => g.fillRect(0, it[0], th, it[1]));//, h-rad-2));
-// Right
-//intervals.forEach(it => g.fillRect(w-th-2, it[0], w, it[1]));
-
-//function 
-// Bottom segment
-//g.setColor(0,255,0);
-//g.fillRect(0+rad,h-th-2, w-rad-2, h);
-// Left segment
-//.setColor(0,0,255);
-//g.fillRect(0,0+rad, th, h-rad-2);
-// Right segment
-//g.setColor(255,0,255);
-//g.fillRect(w-th-2,0+rad, w, h-rad-2);
