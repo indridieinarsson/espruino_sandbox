0c69bdeebad700ecf9a61b79cfcb3a1c02cffa42
Indriði Einarsson <indridi@indridi.net>
2023-12-28 01:17:50 +0000

    refine 24 hour dial

--- 24hAuxDial.js
+++ 24hAuxDial.js
@@ -1,7 +1,6 @@
 ;(function () {
-    let HourHandOffset = 0
-    let MinuteHandOffset = 0
-    let SecondHandOffset = 0;
+    let SecondHandOffset = 2;
+    let SecondHandLength = 0;
 
     let twoPi  = 2*Math.PI, deg2rad = Math.PI/180;
     let Pi     = Math.PI;
@@ -9,40 +8,26 @@
 
     let sin = Math.sin, cos = Math.cos;
 
-    let HourHandLength  = 0;
-    let MinuteHandLength  = 0;
-
     exports.draw = function draw (
         Settings, CenterX, CenterY, outerRadius, Hours,Minutes,Seconds
     ) {
-        HourHandLength = outerRadius * 0.52;
-        MinuteHandLength  = outerRadius * 0.9;
-
-        let HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
-        let MinutesAngle = (Minutes/60)            * twoPi - Pi;
-
-        g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
-        let sPhi = Math.sin(HoursAngle), cPhi = Math.cos(HoursAngle);
-        g.drawLine(
-            CenterX + HourHandOffset*sPhi,
-            CenterY - HourHandOffset*cPhi,
-            CenterX - HourHandLength*sPhi,
-            CenterY + HourHandLength*cPhi
-        );
-
-        sPhi = Math.sin(MinutesAngle), cPhi = Math.cos(MinutesAngle);
-        g.drawLine(
-            CenterX + MinuteHandOffset*sPhi,
-            CenterY - MinuteHandOffset*cPhi,
-            CenterX - MinuteHandLength*sPhi,
-            CenterY + MinuteHandLength*cPhi
-        );
-
         function drawdot(i){
             let Phi = i * Math.PI*2/12;
             let dotx = CenterX + outerRadius * Math.sin(Phi);
             let doty = CenterY - outerRadius * Math.cos(Phi);
             g.drawCircle(dotx,doty, 1);
+        }
+
+	function draw24htime(h, m){
+            let SecondsAngle   = (h+(m/60))/24 * twoPi - Pi;
+            sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
+            let SecondHandLength = outerRadius * 0.9;
+            g.drawLine(
+                CenterX + SecondHandOffset*sPhi,
+                CenterY - SecondHandOffset*cPhi,
+                CenterX - SecondHandLength*sPhi,
+                CenterY + SecondHandLength*cPhi
+            );
         }
         g.setColor('#00FFFF');//Cyan
         for (let i = -2; i < 3; i++) {
@@ -56,20 +41,12 @@
         drawdot(-3);
         drawdot(3);
 
-
-	    // Not seconds, but 24 hour clock
-        if (Seconds != null) {
-            g.setColor(Settings.Seconds === 'Theme' ? g.theme.fgH : Settings.Seconds || '#FF0000');
-            // 24 hour clock
-            let SecondsAngle   = (Hours+(Minutes/60))/24 * twoPi - Pi;
-            sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
-            let SecondHandLength = outerRadius * 0.9;
-            g.drawLine(
-                CenterX + SecondHandOffset*sPhi,
-                CenterY - SecondHandOffset*cPhi,
-                CenterX - SecondHandLength*sPhi,
-                CenterY + SecondHandLength*cPhi
-            );
-        }
+        g.setColor(Settings.Seconds === 'Theme' ? g.theme.fgH : Settings.Seconds || '#FF0000');
+	draw24htime(Hours, Minutes);
+        g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
+	let now = new Date();
+	let Hoursn   = now.getHours() % 12;
+	let Minutesn = now.getMinutes();
+	draw24htime(Hoursn, Minutesn);
     };
 })();
